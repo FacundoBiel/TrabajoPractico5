10c28ad616df9e8ba78c77b3a1d1636de8132a33
FacundoBiel <151086994+FacundoBiel@users.noreply.github.com>
2024-04-14 17:25:56 -0300

    Add files via upload

---
+++ Ejercicio 5 - 2/src/ejercicio/pkg5/pkg2/Main.java
@@ -0,0 +1,8 @@
+package ejercicio.pkg5.pkg2;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ Ejercicio 5 - 2/src/ejercicio/pkg5/pkg2/Producto.java
@@ -0,0 +1,31 @@
+package ejercicio.pkg5.pkg2;
+
+public class Producto {
+    
+    String nombre;
+    int precio;
+
+    public Producto(String nombre, int precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    
+    
+}
